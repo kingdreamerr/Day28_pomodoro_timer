f60bc5b4376ba9e3ba8d5968bd1df525038913c2
kingdreamerr <jackmvula458@gmail.com>
2023-03-03 12:15:42 +0200

    Add bg image using canvas

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ timer = None
 window = Tk()
 window.title("Pomodoro timer")
 window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=203, height=224, bg=YELLOW, highlightthickness=0)
+image = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=image)
+text_time = canvas.create_text(103, 130, text="00:00", fill="white",font=(FONT_NAME, 35, 'bold'))
+canvas.grid(columnspan=3, row=1)
+timer_title = Label(text="Timer", font=(FONT_NAME, 35, 'bold'), bg=YELLOW, fg=GREEN)
+timer_title.grid(column=1, row=0)
